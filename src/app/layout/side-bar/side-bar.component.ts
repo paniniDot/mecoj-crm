7cf8baae56bb43aa71d7354779e5045dcc0b8520
Mattia Panni <pnmattia@gmail.com>
2025-01-15 20:12:33 +0100

    Update popup add event

--- src/app/layout/side-bar/side-bar.component.ts
+++ src/app/layout/side-bar/side-bar.component.ts
@@ -3,13 +3,11 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule], 
+  imports: [RouterModule],
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css'],
+  styleUrls: ['./side-bar.component.css'], 
 })
 export class SidebarComponent {
-  isExpanded = true;
-
   menuItems = [
     { label: 'Dashboard', path: '/dashboard', icon: 'dashboard' },
     { label: 'Clienti', path: '/app-client-list', icon: 'people' },
@@ -17,8 +15,4 @@ export class SidebarComponent {
     { label: 'Calendario', path: '/app-calendar', icon: 'calendar_today' },
     { label: 'Bilanci', path: '/finances', icon: 'account_balance' },
   ];
-
-  toggleSidebar() {
-    this.isExpanded = !this.isExpanded;
-  }
 }
